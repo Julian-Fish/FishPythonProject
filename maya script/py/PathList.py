faca2c76f5906644b8afc29ae25d0b857baac0c4
19cu0217-ShuTeki <19cu0217@jec.ac.jp>
2021-01-19 18:58:21 +0900

    [PathList]Constructの削除

--- maya script/py/PathList.py
+++ maya script/py/PathList.py
@@ -1,12 +1,14 @@
+#2021.1.19 Constructを使ったらクラッシュになる可能があるので、閉じで起きます
+
 SCRIPT_PATH = {
-    "Construct"     : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/Construct.py", 
+    #"Construct"     : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/Construct.py", 
     "UVAlign"       : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/UVAlignVer2.py", 
     "MultiRename"   : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/multiRename.py",
     "SLKeyFObj"     : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/SLKeyFObj.py"
 }
 
 SCRIPT_NAME = [
-    "Construct",
+    #"Construct",
     "UVAlign",
     "MultiRename",
     "SLKeyFObj"
