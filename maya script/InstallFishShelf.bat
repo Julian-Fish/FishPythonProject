993d4a60d8ebb457a7607e0f3d7e4dc1257bfb3e
19cu0217-ShuTeki <19cu0217@jec.ac.jp>
2021-01-19 18:06:00 +0900

    [InstallBatch]コピーの修正

--- maya script/InstallFishShelf.bat
+++ maya script/InstallFishShelf.bat
@@ -2,7 +2,9 @@ set MAYA_Version=2019
 set FISHSHELF_PATH=%UserProfile%\Documents\maya\%MAYA_Version%\prefs\shelves\FishShelf
 
 md %FISHSHELF_PATH%
+md %FISHSHELF_PATH%\qtui
 
-xcopy .\FishShelf %FISHSHELF_PATH% /e /y
+xcopy .\qtui %FISHSHELF_PATH%\qtui /e /y
+xcopy .\FishShelf.mel %FISHSHELF_PATH% /y
 
 pause
