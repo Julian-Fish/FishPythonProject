3f99ac2666cf1c2e6e71d7b0f30e7e9f1cb6b28c
19cu0217-ShuTeki <19cu0217@jec.ac.jp>
2021-01-19 18:37:46 +0900

    [PathList]SLKeyFObjのパスの追加

--- maya script/py/PathList.py
+++ maya script/py/PathList.py
@@ -2,12 +2,14 @@ SCRIPT_PATH = {
     "Construct"     : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/Construct.py", 
     "UVAlign"       : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/UVAlignVer2.py", 
     "MultiRename"   : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/multiRename.py",
+    "SLKeyFObj"     : "https://raw.githubusercontent.com/Julian-Fish/FishPythonProject/master/maya%20script/py/SLKeyFObj.py"
 }
 
 SCRIPT_NAME = [
     "Construct",
     "UVAlign",
     "MultiRename"
+    "SLKeyFObj"
 ]
 
 QTUI_PATH = {
