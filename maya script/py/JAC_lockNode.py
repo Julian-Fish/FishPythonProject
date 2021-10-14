770b37e07b2f2a513c64643ff46334803f67a1b5
JulianFish <zero94516@gmail.com>
2021-10-14 15:01:21 +0900

    add lock/unlock node

--- maya script/py/JAC_lockNode.py
+++ maya script/py/JAC_lockNode.py
@@ -10,7 +10,7 @@ import shutil
 import maya.mel as mel
 
 # 間接パスの指定
-UIFILEPATH = "D:/19cu0217/maya/FishPythonProject/maya script/qtui/JAC_lockNode.ui"
+UIFILEPATH = "D:/19cu0217/FishPythonProject/maya script/qtui/JAC_lockNode.ui"
 workRootDir = mc.workspace(q = 1, rootDirectory = 1)
 
 print workRootDir
@@ -28,32 +28,40 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         
         # try to layout with code
         # create widget
-        FileNameLabel = QLabel("FileName:", self)
+        self.FileNameLabel = QLabel("Excel File:", self)
         self.FileNameValueLabel = QLabel("")
-        BrowseBtn = QPushButton("Brose..")
-        CopyBtn = QPushButton("Copy Map")
+        self.BrowseBtn = QPushButton("Brose..")
+        self.CopyBtn = QPushButton("Copy Map")
+        self.LockBtn = QPushButton("Lock Node")
+        self.UnlockBtn = QPushButton("Unlock Node")
 
         # layout
-        layoutHBox = QHBoxLayout(self)
+        layoutHBox1 = QHBoxLayout()
+        layoutHBox1.addWidget(self.FileNameLabel)
+        layoutHBox1.addWidget(self.FileNameValueLabel)
+        layoutHBox1.addWidget(self.BrowseBtn)
         
-        layoutHBox.addWidget(FileNameLabel)
-        layoutHBox.addWidget(self.FileNameValueLabel)
-        layoutHBox.addWidget(BrowseBtn)
+        layoutHBox2 = QHBoxLayout()
+        layoutHBox2.addWidget(self.LockBtn)
+        layoutHBox2.addWidget(self.UnlockBtn)
         
-        layoutVBox = QVBoxLayout(self)
-        layoutVBox.addLayout(layoutHBox)
-        layoutVBox.addWidget(CopyBtn)
+        layoutVBox = QVBoxLayout()
+        layoutVBox.addLayout(layoutHBox1)
+        layoutVBox.addWidget(self.CopyBtn)
+        layoutVBox.addLayout(layoutHBox2)
                                     
         # self.setLayout(layoutVBox)#
         self.setWindowTitle("JAC")
         
-        widget = QWidget(self)
+        widget = QWidget()
         widget.setLayout(layoutVBox)
         self.setCentralWidget(widget)
         
         # 接続
-        BrowseBtn.clicked.connect(self.openFileDialog)
-        CopyBtn.clicked.connect(self.copyMap)
+        self.BrowseBtn.clicked.connect(self.openFileDialog)
+        self.CopyBtn.clicked.connect(self.copyMap)
+        self.LockBtn.clicked.connect(self.lockNode)
+        self.UnlockBtn.clicked.connect(self.unlockNode)
         # self.UI.BrowseBtn.clicked.connect(self.openFileDialog)
         # self.UI.CopyBtn.clicked.connect(self.copyMap)
         
@@ -78,13 +86,22 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
             #generate authorStr
             authorStr = row[0] + row[1]
             
-            #create author node and set hidden
-            authorNode = mc.group(em = 1, name = "AuthorNode")
+            # 1.add author to attribute
+            # #create author node and set hidden
+            # authorNode = mc.group(em = 1, name = "AuthorNode")
+            # mc.setAttr("AuthorNode.hiddenInOutliner", True)
+            # #add attr and lock it
+            # mc.addAttr("AuthorNode", longName = "Author", dataType = "string")
+            # mc.setAttr("AuthorNode.Author", authorStr, type = "string")
+            # mc.setAttr("AuthorNode.Author", lock = 1)
+            
+            # 2.add author(number) to group name
+            # create author node and set hidden
+            groupName = "Author_" + row[0]
+            print groupName
+            authorNode = mc.group(em = 1, name = groupName)
             mc.setAttr("AuthorNode.hiddenInOutliner", True)
-            #add attr and lock it
-            mc.addAttr("AuthorNode", longName = "Author", dataType = "string")
-            mc.setAttr("AuthorNode.Author", authorStr, type = "string")
-            mc.setAttr("AuthorNode.Author", lock = 1)
+            
             #lock node
             mc.lockNode(authorNode, lock = 1)
             #fresh outliner
@@ -103,7 +120,7 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
             mc.delete(authorNode)
             count += 1
             
-        self.copyCount = count   
+        self.copyCount = str(count)
         self.copyDone()
         
     def copyDone(self):
@@ -117,6 +134,16 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         
         msgBox.exec_()
         
+    def lockNode(self):
+        nodes = mc.ls(sl = 1)
+        for node in nodes:
+            mc.lockNode(node, lock = 1)
+            print node + " locked"
+    def unlockNode(self):
+        nodes = mc.ls(sl = 1)
+        for node in nodes:
+            mc.lockNode(node, lock = 0)
+            print node + " unlocked"
 ## MainWindowの起動
 def main():
     window = MainWindow()
