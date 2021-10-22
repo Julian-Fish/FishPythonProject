b5d21c8dd73f4ff83c28bcb683d7b58353ed9d37
JulianFish <zero94516@gmail.com>
2021-10-22 16:30:25 +0900

    add sign uuid list

--- maya script/py/JAC/JAC_AddUUID.py
+++ maya script/py/JAC/JAC_AddUUID.py
@@ -22,6 +22,8 @@ print targetDir
 class CopyWork(QObject):
     finished = Signal()
     progress = Signal(int)
+    uuidList = Signal(list)
+
     def __init__(self, table, count, parent=None):
         super(CopyWork, self).__init__(parent)
         
@@ -29,11 +31,15 @@ class CopyWork(QObject):
         self.table = table
 
     def copyMap(self):
-        #generate authorStr
+        #generate authorStr and uuid
+        _uuidList = []
         for i in range(0, self.count):
-            mu.executeInMainThreadWithResult(self.doInMain, i)
+            uuid = mu.executeInMainThreadWithResult(self.doInMain, i)
+            _uuidList.append(uuid[0])
             self.progress.emit(i + 1)
+        self.uuidList.emit(_uuidList)
         self.finished.emit()
+        
         
     def doInMain(self, i):
         number = self.table[i][0]
@@ -50,11 +56,16 @@ class CopyWork(QObject):
         # mc.setAttr("AuthorNode.Author", lock = 1)
         
         # 2.add author(number) to group name
-        # create author node and set hidden
+        # create author node ,get uuid and  set hidden
         groupName = "Author_" + number
         authorNode = mc.group(em = 1, name = groupName)
-        mc.setAttr(groupName + ".hiddenInOutliner", True)
+        uuid = mc.ls(authorNode, uuid = 1)
+        mc.setAttr(authorNode + ".hiddenInOutliner", True)
         
+        #add origin uuid to attribute
+        mc.addAttr(authorNode,longName = "signUUID", dataType = "string")
+        mc.setAttr(authorNode + ".signUUID", uuid[0], type = "string")
+
         #lock node
         mc.lockNode(authorNode, lock = 1)
         #fresh outliner
@@ -75,7 +86,8 @@ class CopyWork(QObject):
         mc.lockNode(authorNode, lock = 0)
         mc.delete(authorNode)
         
-        return groupName
+        #uuid as result
+        return uuid
 
 ## MainWindowを作るクラス
 class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
@@ -97,7 +109,7 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         self.Browse2Btn = QPushButton("Browse..")
         self.isDefaultDir = True
 
-        self.CopyBtn = QPushButton("Copy Map")
+        self.CopyBtn = QPushButton("Copy Map && Generate UUID")
         self.LockBtn = QPushButton("Lock Node")
         self.UnlockBtn = QPushButton("Unlock Node")
 
@@ -123,13 +135,25 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         excelViewHBox = QHBoxLayout()
         #   sheet list
         sheetVBox = QVBoxLayout()
-        sheetVBox.setFixedSize(200, 300)
         self.sheetListWidget = QListWidget()
-        newSheetBtn = QPushButton("New Sheet")
+        self.sheetListWidget.setFixedSize(200, 300)
+        #   create sheet button
+        newSheetHBox = QHBoxLayout()
+        self.newSheetLineEdit = QLineEdit()
+        newSheetBtn = QPushButton("+")
+        newSheetHBox.addWidget(self.newSheetLineEdit)
+        newSheetHBox.addWidget(newSheetBtn)
+        newSheetBtn.clicked.connect(self.createSheet)
+
         sheetVBox.addWidget(self.sheetListWidget)
-        sheetVBox.addWidget(newSheetBtn)
+        sheetVBox.addLayout(newSheetHBox)
+        #sheetVBox.addWidget(newSheetBtn)
+        self.sheetListWidget.currentItemChanged.connect(self.loadTable)
         #   table
         self.tableWidget = QTableWidget()
+        headerLabels = [u"学籍番号", u"名前", u"UUID"]
+        self.tableWidget.setColumnCount(len(headerLabels))
+        self.tableWidget.setHorizontalHeaderLabels(headerLabels)
 
         excelViewHBox.addLayout(sheetVBox)
         excelViewHBox.addWidget(self.tableWidget)
@@ -183,16 +207,58 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
     def browseExcelFile(self):
         #open file dialog
         fileDir = QFileDialog.getOpenFileName(self, "Open Excel", "", "Excel Files(*.xlsx)")
+        self.currentExcelFileDir = fileDir[0]
+
         splitStr = fileDir[0].split("/")
         fileName = splitStr[-1]
         #print str[-1]
         # self.UI.FileNameValue.setText(fileName)
-        self.FileNameValueLabel.setText(fileName)
-        #load file
-        self.wb = load_workbook(fileDir[0])
+        if fileName != "":
+            self.FileNameValueLabel.setText(fileName)
+            #load file
+            self.wb = load_workbook(fileDir[0])
+            self.loadSheet()
+
+    def loadSheet(self):
+        self.tableWidget
+        
+        self.sheetListWidget.clear()
+        sheetList = self.wb.sheetnames
+
+        for v in sheetList[1::]:
+            item = QListWidgetItem(v)
+            self.sheetListWidget.addItem(item)
+
+    def loadTable(self):
+        self.tableWidget.clearContents()
+
+        sheetName = self.sheetListWidget.currentItem().text()
+        print sheetName
+
+        ws = self.wb[sheetName]
+        rowCount = len(tuple(ws.rows))
+        self.tableWidget.setRowCount(rowCount)
+
+        for row, rowData in enumerate(ws.values):
+            if rowData[0] is None:
+                break
+            for col, data in enumerate(rowData):
+                item = QTableWidgetItem(data)
+                self.tableWidget.setItem(row, col, item)
     
+    def createSheet(self):
+        newSheetName = self.newSheetLineEdit.text()
+        source = self.wb[self.wb.sheetnames[0]]
+        ws = self.wb.copy_worksheet(source)
+        ws.title = newSheetName
+        self.save()
+
+        item = QListWidgetItem(newSheetName)
+        self.sheetListWidget.addItem(item)
+        self.sheetListWidget.setCurrentItem(item)
+ 
     def copyMap(self):
-        ws = self.wb[self.wb.sheetnames[0]]
+        ws = self.wb[self.sheetListWidget.currentItem().text()]
         self.count = 0
         for row in ws.values:
             if row[0] is None:
@@ -210,9 +276,10 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         self.copyWork = CopyWork(table, self.count)
         #disable button to avoid double click(crush issue)
         self.CopyBtn.setEnabled(False)
-        #self.copyWork.copyMap()  # run without thread for test
+        self.copyWork.copyMap()  # run without thread for test
         
         self.copyWork.moveToThread(self.thread)
+        self.copyWork.uuidList.connect(self.emitUuidList)
         self.copyWork.progress.connect(self.emitProgress)
         self.copyWork.finished.connect(self.copyDone)
         self.copyWork.finished.connect(self.thread.quit)
@@ -226,6 +293,20 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         self.thread.finished.connect(self.thread.deleteLater)
         self.thread.start()
     
+    def emitUuidList(self, uuidList):
+        #print uuidList
+        currentSheet = self.sheetListWidget.currentItem().text()
+
+        #input uuid to col 3
+        ws = self.wb[currentSheet]
+        uuidCol = tuple(ws["C"])
+
+        for i in range(0, len(uuidList)):
+            uuidCol[i].value = uuidList[i]
+
+        self.save()
+        self.loadTable()
+
     def emitProgress(self, p):
         self.copiedCountStr = str(p)
         label = self.copiedCountStr + "/" + str(self.count)
@@ -233,7 +314,6 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         self.ProgressDialog.setValue(p)
     
     def copyDone(self):
-        print "copy done"
         msgBox = QMessageBox()
         msgBox.setWindowTitle("Done")
         msgBox.setText(r"<b>Copy Done<b>")
@@ -254,6 +334,10 @@ class MainWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         for node in nodes:
             mc.lockNode(node, lock = 0)
             print node + " unlocked"
+
+    def save(self):
+        self.wb.save(self.currentExcelFileDir)
+
 ## MainWindowの起動
 def main():
     window = MainWindow()
